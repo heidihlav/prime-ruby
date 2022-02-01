ef409889852a3a0ab8654f9033ffd7bdfa461b6d
Heidi Hlavinka <hchlav@gmail.com>
2022-01-31 20:31:39 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+    number % n == 0
+    if number is prime return true
+    else number is not prime return false
+end
+
+# A prime number is a number other than 1 that is only divisible by itself and 1. 
+# the prime numbers between 1 and 10 are 2, 3, 5, and 7
+# retur true or false 
+
+# (1..10).each do |i|
+#     if i > 1 && i % self == 0 use ruby self?
