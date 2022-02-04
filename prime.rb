69158d6b20b753ee10f147a886eb82b5e279f409
Heidi Hlavinka <hchlav@gmail.com>
2022-02-03 19:08:54 -0600

    commit tree 3a7003872efb48d87744df65a440da45294016aa

--- prime.rb
+++ prime.rb
@@ -1,31 +1,12 @@
 require 'pry'
 
 def prime?(number)
-    number.each do |i|
-        if i > 1 && i % i == 0
-            puts "1 eat a cookie"
-        else
-            puts "this is horseshit"
-        end
+    if number <= 1
+        return false
+    else (2..number - 1).each do |num|
+        return false if number % num == 0
     end
-    # number % n == 0
-
-    # if number is prime return true
-    # else number is not prime return false
+        true
+    end
 end
 
-# A prime number is a number other than 1 that is only divisible by itself and 1. 
-# the prime numbers between 1 and 10 are 2, 3, 5, and 7
-# retur true or false 
-
-# (1..10).each do |i|
-#     if i > 1 && i % i == 0
-#         binding.pry
-#         puts "1 eat a cookie"
-#     else
-#         puts "this is horseshit"
-#     end
-#  end
-    
-
-#  binding.pry
